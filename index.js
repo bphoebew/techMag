4dce9cd7ff02600b06ee3315877fa03c6dcc4409
bphoebew <bphoebew@gmail.com>
2019-11-24 13:02:12 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginRight = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight = "0";
+}
